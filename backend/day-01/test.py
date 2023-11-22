599d8648edc05b3abec60e3bdc883d891672e354
Jhon Carlos Naval <148066540+JCutiepi3@users.noreply.github.com>
2023-11-22 20:38:26 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,7 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "JC Naval"
+age = 20
+isMale = "male"
+department = "DSWD"
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"Hoping that he may learn something new in this 30 Days.")
