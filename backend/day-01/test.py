07a0524552bd35795f7024922f40da6ac2f00af5
Jhon Carlos Naval <148066540+JCutiepi3@users.noreply.github.com>
2023-11-22 20:52:48 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,7 +1,9 @@
 name = "JC Naval"
-age = 20
-isMale = "male"
+information = " I am fresh 20 year old that is quiet, \nbut is open to talk too ^-^"
+other_info = "If anyone is interested to play League or Apex HMU!!"
 department = "DSWD"
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"Hello, I am {name}, and I love {department} and I love being a backend even more!")
+print(f"One thing about me is that {information}")
+print(f"Further more {other_info}")
 print(f"Hoping that he may learn something new in this 30 Days.")
