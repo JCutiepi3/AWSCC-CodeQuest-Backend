07c1c988a9281296e7d05390e6175b0f4d0d24a8
Jhon Carlos Naval <148066540+JCutiepi3@users.noreply.github.com>
2023-11-24 20:21:15 +0800

    Update day-2.py
    Finish Challenge

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,7 @@
+first_number = int (input("Enter the first number: "))
+second_number =int(input("Enter the Second number:"))
+third_number= int(input("Enter the third number:"))
+
+sum = (first_number + second_number + third_number)
+
+print(f"\nSum: {sum}")
